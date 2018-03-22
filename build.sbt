ccb1eb87ef6f7e9ec896933aa670622afbabbfbf
syjsmk <syjsmk@naver.com>
2018-03-23 01:57:34 +0900

    라이브러리 변경 및 url 설명

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "DCConCrawler"
 version := "0.1"
 
 scalaVersion := "2.12.5"
+
+libraryDependencies ++= Seq(
+  "org.scalaj" % "scalaj-http_2.12" % "2.3.0"
+)
--- src/main/scala/DCConCrawler.scala
+++ src/main/scala/DCConCrawler.scala
@@ -1,16 +1,38 @@
-object DCConCrawler {
+import scalaj.http._
 
-  def get(url: String) = {
-    io.Source.fromURL(url).mkString
-  }
+object DCConCrawler {
 
   def main(args: Array[String]): Unit = {
 
-//    val url = "http://naver.com"
-//    val url = "http://www.dcinside.com/"
-    val url = "http://mall.dcinside.com/?from=A08"
 
-    println(get(url))
+    /*
+
+      http://mall.dcinside.com/?from=A08#6312
+      http://dccon.dcinside.com/index/package_detail
+      http://dccon.dcinside.com/#6312
+
+
+      참고
+
+      http://dccon.dcinside.com/hot/5
+      http://dccon.dcinside.com/new/1#35139
+
+      http://~~~/
+      hot : 인기 디시콘
+      new : 신상 디시콘
+      #뒤 번호 : 디시콘 번호
+
+      순서대로 검색하고자 한다면 new를 쓰는 것이 나을듯
+
+      url/new/1 로 GET요쳥을 보냈을 때 해당 html의 package_idx값이 해당 디시콘의 url이 됨.
+
+     */
+
+
+//    val DCConPackageUrl = "http://dccon.dcinside.com/new/1#35139"
+    val DCConPackageUrl = "http://dccon.dcinside.com/new/1#35148"
+
+    println(Http(DCConPackageUrl).asString)
 
 
   }
