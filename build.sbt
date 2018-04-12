d6e1e4529c026bad566b8827c822cd74ffb8862a
syjsmk <syjsmk@naver.com>
2018-04-13 00:58:22 +0900

    Get json data about DCCon from DC

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ version := "0.1"
 scalaVersion := "2.12.5"
 
 libraryDependencies ++= Seq(
-  "org.scalaj" % "scalaj-http_2.12" % "2.3.0"
+  "org.scalaj" % "scalaj-http_2.12" % "2.3.0",
+  "com.typesafe.play" %% "play-json" % "2.6.7"
 )
--- src/main/scala/DCConCrawler.scala
+++ src/main/scala/DCConCrawler.scala
@@ -1,9 +1,9 @@
+import play.api.libs.json.{JsObject, JsValue, Json}
 import scalaj.http._
 
 object DCConCrawler {
 
   def main(args: Array[String]): Unit = {
-
 
     /*
 
@@ -26,14 +26,51 @@ object DCConCrawler {
 
       url/new/1 로 GET요쳥을 보냈을 때 해당 html의 package_idx값이 해당 디시콘의 url이 됨.
 
+
+      // 아래의 주소로 POST를 보낼 때
+      http://dccon.dcinside.com/index/package_detail
+
+      curl --data "ci_t=75960e76856b01dc69a224891f4874bd&package_idx=15276&dcConResponse=" http://dccon.dcinside.com/index/package_detail
+      위의 인자와 함께 POST를 보낼 경우 반응이 있긴 하지만 허락되지 않은 POST라는 결과가 나옴. cookie 문제인듯.
+
+      curl -X GET "http://dcimg5.dcinside.com/dccon.php?no=62b5df2be09d3ca567b1c5bc12d46b394aa3b1058c6e4d0ca41648b65ceb246e13df9546348593b9b03a53cb28363e94746ccdd8268733d87a9837c6e3cdbd8dab5749" -H 'Referer: http://dccon.dcinside.com/' > t.png
+
+
+      이 URI로 curl 요청하면 해당 idx의 DC콘 정보가 json으로 들어옴
+      // curl 'http://dccon.dcinside.com/index/package_detail' -H 'Cookie: ci_c=61e286cd35e229c36c8d24d17e4289fe' -H 'X-Requested-With: XMLHttpRequest' --data 'ci_t=61e286cd35e229c36c8d24d17e4289fe&package_idx=15276&dcConResponse=' --compressed
+      // curl 'http://dccon.dcinside.com/index/package_detail' -H 'Cookie: ci_c=61e286cd35e229c36c8d24d17e4289fe' -H 'X-Requested-With: XMLHttpRequest' --data 'ci_t=61e286cd35e229c36c8d24d17e4289fe&package_idx=15276&dcConResponse='
+
      */
 
 
-//    val DCConPackageUrl = "http://dccon.dcinside.com/new/1#35139"
-    val DCConPackageUrl = "http://dccon.dcinside.com/new/1#35148"
+    val DCConPackageUrl = "http://dccon.dcinside.com/index/package_detail"
 
-    println(Http(DCConPackageUrl).asString)
+    // ci_c랑 ci_t를 새로 안만들고 저거 계속 써도 되는지 모르겠음
+    val data = "ci_t=61e286cd35e229c36c8d24d17e4289fe&package_idx=15276&dcConResponse="
 
+    val dcConResponse = Http(DCConPackageUrl)
+      .postData(data)
+      .header("Cookie", "ci_c=61e286cd35e229c36c8d24d17e4289fe")
+      .header("X-Requested-With", "XMLHttpRequest").asString
+
+    //    println(Http(DCConPackageUrl).asString)
+    println(dcConResponse)
+    println(dcConResponse.body)
+
+    val dcConJson: JsValue = Json.parse(dcConResponse.body)
+
+//    println(dcConJson)
+
+    // \ 로 현재 노드 바로 아래 단계를 탐색함
+    println(((dcConJson \ "info") \ "package_idx").as[String])
+
+    // \\로 JsValue 내에서 find 기능 수행
+    (dcConJson \\ "package_idx").foreach(packageIdx => {
+      println(packageIdx)
+    })
+
+
+    println(Json.toJson(dcConResponse.body))
 
   }
 }
